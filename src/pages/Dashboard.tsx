b3d7de51ac461a73a128c4f41349e43dfce544b1
Hamza Simsek <hamzas4011@hotmail.com>
2026-01-19 00:13:06 +0100

    update dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,14 @@
 export default function Dashboard() {
-    return <h1>Dashboard</h1>;
+    return (
+      <main className="max-w-xl mx-auto">
+        <h1 className="text-2xl font-bold mb-2">
+          Daily wellbeing check-in
+        </h1>
+  
+        <p className="mb-6 text-gray-600">
+          Take a moment to reflect on how you are feeling today.
+        </p>
+      </main>
+    );
   }
   
