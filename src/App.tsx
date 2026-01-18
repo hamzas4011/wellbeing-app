cde867a6e2698176965ec2b3dd1d0dadc6499eb8
Hamza Simsek <hamzas4011@hotmail.com>
2026-01-18 20:40:15 +0100

    files fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-3xl font-bold">
-        Accessible Wellbeing App
+        Wellbeing App
       </h1>
     </main>
   );
